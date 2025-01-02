d63686d0cecc008e1f4d9cf83879bddfd8f763a9
Annguyn <nguyenbaxuanan.pdr.888@gmail.com>
2025-01-03 06:12:19 +0700

    fix statistics by hour

--- api/sensor_api.py
+++ api/sensor_api.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 
+from torch.distributed.argparse_util import env
+
 sensor_api = Blueprint('sensor_api', __name__)
 load_dotenv()
 ESP32_IP = os.getenv('ESP32_IP')
@@ -38,14 +40,26 @@ def statistics():
             parameters = (start_value.strftime('%Y-%m-%d %H:%M:%S'), end_value.strftime('%Y-%m-%d %H:%M:%S'))
         elif filter_type == 'hour':
             start_value = datetime.strptime(filter_value.split('.')[0], '%Y-%m-%dT%H:%M:%S')
-            end_value = start_value + timedelta(hours=1)
+            end_value = start_value - timedelta(hours=1)
             query = "SELECT * FROM sensor_status WHERE timestamp >= ? AND timestamp < ?"
-            parameters = (start_value.strftime('%Y-%m-%d %H:%M:%S'), end_value.strftime('%Y-%m-%d %H:%M:%S'))
+            parameters = (end_value.strftime('%Y-%m-%d %H:%M:%S'), start_value.strftime('%Y-%m-%d %H:%M:%S'))
         elif filter_type == 'month':
             start_value = datetime.strptime(filter_value, '%Y-%m-%d')
             end_value = (start_value + timedelta(days=32)).replace(day=1)
             query = "SELECT * FROM sensor_status WHERE timestamp >= ? AND timestamp < ?"
             parameters = (start_value.strftime('%Y-%m-01 00:00:00'), end_value.strftime('%Y-%m-01 00:00:00'))
+        elif filter_type == 'week':
+            start_value = datetime.strptime(filter_value, '%Y-%m-%d')
+            start_value = start_value - timedelta(days=start_value.weekday())
+            end_value = start_value + timedelta(days=7)
+            query = "SELECT * FROM sensor_status WHERE timestamp >= ? AND timestamp < ?"
+            parameters = (start_value.strftime('%Y-%m-%d %H:%M:%S'), end_value.strftime('%Y-%m-%d %H:%M:%S'))
+        elif filter_type == 'year':
+            start_value = datetime.strptime(filter_value, '%Y-%m-%d')
+            start_value = start_value.replace(month=1, day=1)
+            end_value = start_value.replace(year=start_value.year + 1)
+            query = "SELECT * FROM sensor_status WHERE timestamp >= ? AND timestamp < ?"
+            parameters = (start_value.strftime('%Y-%m-%d %H:%M:%S'), end_value.strftime('%Y-%m-%d %H:%M:%S'))
         else:
             return jsonify({'error': 'Invalid filter type'}), 400
     except ValueError as e:
@@ -77,4 +91,3 @@ def statistics():
 
     conn.close()
     return jsonify(data)
-
